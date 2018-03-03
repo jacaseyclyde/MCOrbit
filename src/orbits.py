551487b8baecbe8f2c3950aaafba6e7b057d81fd
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-03-02 17:35:27 -0800

    realised that the velocity vector should be able to be found via the same rotations as the position vector

--- src/orbits.py
+++ src/orbits.py
@@ -54,8 +54,7 @@ Menc = Mdat[:,1] # [log(Msun)]
 # =============================================================================
 # Other 
 # =============================================================================
-tspace=1000
-ttest=np.linspace(0 , 2 * np.pi , tspace)
+tstep=1000
 
 
 # =============================================================================
@@ -92,7 +91,7 @@ def Orbit(x0,v0,tstep):
     x0 = [pc], v0 = [km/s], tstep = [yr]
     '''
     npoints = 100 # right now this is a completely arbitraty number
-    pos = np.zeros((npoints,2))
+    pos = np.zeros((npoints,3))
     vel = np.zeros_like(pos)
     
     pos[0] = x0 # [pc]
@@ -132,66 +131,24 @@ def OrbitFunc(p):
 
     (aop, loan, inc, x0, v0)=p
     
-    T = Transmat(p)
-    
-    # k is the relative velocity weight
-    # Msun and G are currently just based on wikipedia and probably need a better source
-    Mdyn = 4.02e+6 # +/- 0.16 +/- 0.04 x 10^6 M_sun
-    Msun = 1.99e+30 # kg
-#    k=1e-1
-    
-     # conversion factor for going from pc to km. Wikipedia
-    
-    #a = a * pcToKm
-    
-    # should probably get better numbers than general googling but this works for now
-    GM = G * Mdyn * Msun / 1e+9 # last part converting from m^3 to km^3
-    
-    # Sanity Check
-    if e >=1.:
-        return np.inf
-    
-    b=a*np.sqrt(1-e**2.) 
-    x0=a*e
-    
-    # reduced angular momentum
-    l = (a - a * e) * np.sqrt(GM * (1 + e) / (a * (1 - e)))
-    
-    '''
-    Generate Ellipse
-    '''    
-    # Parametric coordinates
-    xtest=-x0+a*np.cos(ttest)
-    ytest=b*np.sin(ttest)
-    ztest=np.zeros_like(xtest)
-    rtest = np.sqrt(xtest**2 + ytest**2)
-    
-    sqrtArg = ((GM * (2 * a - rtest)) / (a * rtest)) - (l / rtest)**2
-    
-    if ( sqrtArg < 0.).any():
-        sqrtArg = 0. # just doing a hard cuttoff, since i'm about 90% sure that values less than 0 are due to roundoff
-        
-    xdottest = -((ytest * l) / rtest**2) + (xtest / rtest) * np.sqrt(sqrtArg)
-    ydottest = ((xtest * l) / rtest**2) + (ytest / rtest) * np.sqrt(sqrtArg)
-                
-    Vtest = (np.sin(inc) * np.sin(aop) * xdottest) + (-np.sin(inc) * np.cos(aop) * ydottest)
-    
-    # Transform from Orbit to Ellipse
-    rtest=np.vstack([xtest,ytest,ztest])
-    rtest = rtest / kmPc
-    Rtest=np.matmul(np.linalg.inv(T),rtest)
+    T = Transmat(aop, loan, inc)
+    orbr, orbv = Orbit(x0, v0, tstep)
+             
+    # Transform from Orbit plane to Sky plane
+    skyR = np.matmul(np.linalg.inv(T), orbr)
+    skyV = np.matmul(np.linalg.inv(T), orbv)
+    losV = skyV[-1]
             
-    Rtest[2] = Vtest
+    model = np.array([skyR[0],skyR[1],losV])
             
-    return Rtest.T # returning the transpose so that each point on the ellipse
+    return model.T # returning the transpose so that each point on the ellipse
                    # can be treated discretely
 
-def Transmat(p):
+def Transmat(aop, loan, inc):
     """
     Returns the Translation matrix and its derivatives. p must be given in
     radians.
     """
-    (aop, loan, inc)=p
     T = np.array([[np.cos(loan)*np.cos(aop) - np.sin(loan)*np.sin(aop)*np.cos(inc),
         - np.sin(loan)*np.cos(aop) - np.cos(loan)*np.sin(aop)*np.cos(inc), 
         np.sin(aop)*np.sin(inc)],
