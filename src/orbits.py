b1b635e9be3ccdfd55ada4e33ef753d585e7a031
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-03-16 10:59:51 -0700

    finished reorganization of orbits

--- src/orbits.py
+++ src/orbits.py
@@ -18,7 +18,6 @@ import matplotlib.pyplot as plt
 
 np.set_printoptions(precision=5, threshold=np.inf)
 
-datafile = '../dat/CLF-Sim.csv'
 outpath = '../out/'
 
 # =============================================================================
@@ -60,42 +59,7 @@ tstep = 1000
 # # Functions
 # =============================================================================
 # =============================================================================
-
-def Orbit(x0, v0, tstep):
-    '''
-    Takes in an initial position and velocity vector and generates an
-    integrated orbit around SgrA*. Returns 2 arrays of position and veolocity
-    vectors.
-
-    x0 = [pc], v0 = [km/s], tstep = [yr]
-    '''
-
-    npoints = 100  # right now this is a completely arbitraty number
-    pos = np.zeros((npoints, 3))
-    vel = np.zeros_like(pos)
-
-    pos[0] = x0  # [pc]
-    vel[0] = v0 * secYr / kmPc  # [pc/yr]
-
-    posnorm = np.linalg.norm(pos[0])  # [pc]
-    a_old = - G * MassFunc(posnorm) / posnorm**2  # [pc/yr^2]
-    a_old = a_old * pos[0] / posnorm
-
-    for i in range(npoints - 1):
-        pos[i+1] = pos[i] + vel[i] * tstep + 0.5 * a_old * tstep**2
-
-        posnorm = np.linalg.norm(pos[i+1])
-        a_new = - G * MassFunc(posnorm) / posnorm**2
-        a_new = a_new * pos[i+1] / posnorm
-
-        vel[i+1] = vel[i] + 0.5 * (a_old + a_new) * tstep
-
-        a_old = a_new
-
-    return pos, (vel / secYr * kmPc)  # [pc], [km/s]
-
-
-def MassFunc(dist):
+def mass_func(dist):
     '''
     Takes in a distance from SgrA* and returns the enclosed mass. Based on data
     from Feldmeier-Krause et al. (2016), with interpolation
@@ -105,9 +69,9 @@ def MassFunc(dist):
     return 10**np.interp(dist, Mdist, Menc)  # [Msun]
 
 
-def Transmat(aop, loan, inc):
+def rot_mat(aop, loan, inc):
     """
-    Returns the Translation matrix
+    Returns the rotation matrix for going from the orbit plane to the sky plane
 
     aop = [rad], loan = [rad], inc = [rad]
     """
@@ -131,26 +95,41 @@ def Transmat(aop, loan, inc):
     return T
 
 
-def SkytoOrb(X, Y, p):
-    """
-    Takes in sky coods, X,Y, and V, as well as parameters, p, in radians. Calc-
-    ulates the transformation matrix T. Returns the Orbit coords x and y as
-    well as their associated velocities vx and vy
-    """
-    (aop, loan, inc, a, e) = p
+def orbit(x0, v0, tstep):
+    '''
+    Takes in an initial position and velocity vector and generates an
+    integrated orbit around SgrA*. Returns 2 arrays of position and veolocity
+    vectors.
 
-    T = Transmat(p)
+    x0 = [pc], v0 = [km/s], tstep = [yr]
+    '''
 
-    x = T[0][2] / np.cos(inc) * (- T[2][1] * Y - T[2][0] * X) + T[0][1] * Y
-    + T[0][0] * X
+    npoints = 100  # right now this is a completely arbitraty number
+    pos = np.zeros((npoints, 3))
+    vel = np.zeros_like(pos)
 
-    y = T[1][2] / np.cos(inc) * (- T[2][1] * Y - T[2][0] * X) + T[1][1] * Y
-    + T[1][0]*X
+    pos[0] = x0  # [pc]
+    vel[0] = v0 * secYr / kmPc  # [pc/yr]
 
-    return x, y, T
+    posnorm = np.linalg.norm(pos[0])  # [pc]
+    a_old = - G * mass_func(posnorm) / posnorm**2  # [pc/yr^2]
+    a_old = a_old * pos[0] / posnorm
+
+    for i in range(npoints - 1):
+        pos[i+1] = pos[i] + vel[i] * tstep + 0.5 * a_old * tstep**2
+
+        posnorm = np.linalg.norm(pos[i+1])
+        a_new = - G * mass_func(posnorm) / posnorm**2
+        a_new = a_new * pos[i+1] / posnorm
+
+        vel[i+1] = vel[i] + 0.5 * (a_old + a_new) * tstep
+
+        a_old = a_new
+
+    return pos, (vel / secYr * kmPc)  # [pc], [km/s]
 
 
-def Sky(p):
+def sky(p):
     """
     Takes in coordinates ~p in degrees~ and spits out f1 -- the constraint that
     the orbit must be elliptical and SgrA* lies at the focus
@@ -163,12 +142,12 @@ def Sky(p):
     loan = loan * np.pi / 180.
     inc = inc * np.pi / 180.
 
-    T = Transmat(aop, loan, inc)
-    orbr, orbv = Orbit(x0, v0, tstep)
+    rot = rot_mat(aop, loan, inc)
+    orbr, orbv = orbit(x0, v0, tstep)
 
     # Transform from Orbit plane to Sky plane
-    skyR = np.matmul(np.linalg.inv(T), orbr.T)
-    skyV = np.matmul(np.linalg.inv(T), orbv.T)
+    skyR = np.matmul(rot.T, orbr.T)
+    skyV = np.matmul(rot.T, orbv.T)
     losV = skyV[-1]
 
     model = np.array([skyR[0], skyR[1], losV])
@@ -176,68 +155,32 @@ def Sky(p):
     return model.T  # return transpose to get individual ellipse points
 
 
-def PlotFunc(p):
-    my_data = np.genfromtxt(datafile, delimiter=',')
-
-    X = my_data[:, 0]
-    Y = my_data[:, 1]
-    # V=my_data[:,2]
-
-    (aop, loan, inc, a, e) = p
-    b = a * np.sqrt(1 - e**2)
-    x0 = a * e
-    # Mdyn=4.02e+6# +/- 0.16 +/- 0.04 ? 10^6 M_sun
-
-    x, y, T = SkytoOrb(X, Y, p)
-
-    t = np.copy(ttest)
-
-    xx = - x0 + a * np.cos(t)
-    yy = b * np.sin(t)
-    zz = np.zeros_like(xx)
-
-    rr = np.vstack([xx, yy, zz])
-    RR = np.matmul(np.linalg.inv(T), rr)
-
-    XX = RR[0, :]
-    YY = RR[1, :]
-
-    x0true = 0.5 * .9
-    btrue = 0.5 * np.sqrt(1-0.9**2)
-
-    xxtrue = - x0true + 0.5 * np.cos(t)
-    yytrue = btrue * np.sin(t)
-    zztrue = np.zeros_like(xxtrue)
-    rrtrue = np.vstack([xxtrue, yytrue, zztrue])
-
-    Ttrue = Transmat([np.radians(60), np.radians(135), np.radians(300), 0.5,
-                      0.9])
-    Rtrue = np.matmul(np.linalg.inv(Ttrue), rrtrue)
-    Xtrue = Rtrue[0, :]
-    Ytrue = Rtrue[1, :]
-
-    p[0:3] = np.degrees(p[0:3])
+def plot_func(orb, sky):
     plt.figure(1)
-    plt.clf()
-    plt.plot(X, Y, 'ro', XX, YY, 'k-', Xtrue, Ytrue, 'c--', [0], [0], 'g*')
+    plt.plot(sky[:, 0], sky[:, 1], 'k-', label='Gas core')
+    plt.plot([0], [0], 'g*', label='Sgr A*')
+
     plt.grid(True)
     plt.axes().set_aspect('equal', 'datalim')
     plt.title('Sky Plane')  # . p = {}'.format(p))
     plt.xlabel('Offset (pc)')
     plt.ylabel('Offset (pc)')
     plt.gca().invert_xaxis()
-    plt.savefig(outpath + stamp + 'skyplane.pdf', bbox_inches='tight')
+    plt.savefig(outpath + 'skyplane.pdf', bbox_inches='tight')
+    plt.legend()
     plt.show()
+
     plt.figure(2)
-    plt.clf()
-    plt.plot(x, y, 'bx', xx, yy, 'k-', [0], [0], 'g*')
+    plt.plot(orb[:, 0], orb[:, 1], 'k-', label='Gas core')
+    plt.plot([0], [0], 'g*', label='Sgr A*')
+
     plt.grid(True)
     plt.axes().set_aspect('equal', 'datalim')
     plt.title('Orbit Plane')  # . p = {}'.format(p))
     plt.xlabel('Offset (pc)')
     plt.ylabel('Offset (pc)')
-    plt.savefig(outpath + stamp + 'orbitplane.pdf', bbox_inches='tight')
+    plt.savefig(outpath + 'orbitplane.pdf', bbox_inches='tight')
+    plt.legend()
     plt.show()
-    p[0:3] = np.radians(p[0:3])
 
     return
