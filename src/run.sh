be522f51bd673449bc2a00cc46bf61bc0e2f846d
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-08-14 13:48:05 -0700

    Updates to runfile. Still in test mode

--- src/run.sh
+++ src/run.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
+#SBATCH --partition=nodes
+#SBATCH -n 10
 #SBATCH --time=00:01:00
 #SBATCH --job-name=CNDFIT
 #SBATCH --output=CND.out
+#SBATCH --error=CND.err
+
+module purge
 
 module load intel-python3
 module load mvapich2-2.2/intel
+
 mpiexec python main.py
+
