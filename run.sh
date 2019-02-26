47f7511b632fb490efeff31c7af66ddfc78298c8
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2019-02-26 01:12:52 -0800

    Test Run. 250 walkers. 2500 iterations.

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH --partition=nodes
-#SBATCH -n 500
+#SBATCH -n 250
 #SBATCH --time=10-00:00:00
-#SBATCH --job-name=cnd_500w10000i
-#SBATCH --output=cnd_500w.out
-#SBATCH --error=cnd_500w.err
+#SBATCH --job-name=cnd_250w2500i
+#SBATCH --output=cnd_250w.out
+#SBATCH --error=cnd_250w.err
 #SBATCH --mail-user=james.casey-clyde@sjsu.edu
 #SBATCH --mail-type=ALL
 
@@ -13,4 +13,4 @@ module purge
 module load intel-python3
 module load mvapich2-2.2/intel
 
-mpiexec python3 mcorbit/main.py --mpi -d ~/MCOrbit/dat/ --nmax 10000 --walkers 500
+mpiexec python3 mcorbit/main.py --mpi -d ~/MCOrbit/dat/ --nmax 2500 --walkers 250
