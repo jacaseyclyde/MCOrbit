998fce96643265e90a5e578d89db85d454204a39
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2019-02-26 00:21:09 -0800

    Test run. 500 walkers. 10000 iterations.

--- mcorbit/run.sh
+++ mcorbit/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH --partition=nodes
-#SBATCH -n 100
+#SBATCH -n 500
 #SBATCH --time=10-00:00:00
-#SBATCH --job-name=CNDFIT
-#SBATCH --output=CND.out
-#SBATCH --error=CND.err
+#SBATCH --job-name=cnd_500w10000i
+#SBATCH --output=cnd_500w.out
+#SBATCH --error=cnd_500w.err
 #SBATCH --mail-user=james.casey-clyde@sjsu.edu
 #SBATCH --mail-type=ALL
 
@@ -13,4 +13,4 @@ module purge
 module load intel-python3
 module load mvapich2-2.2/intel
 
-mpiexec python3 main.py --mpi -d ~/MCOrbit/dat/ --nmax 50000 --walkers 100
+mpiexec python3 main.py --mpi -d ~/MCOrbit/dat/ --nmax 10000 --walkers 500
