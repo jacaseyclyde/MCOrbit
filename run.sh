61056eacde64228a65e029e35fefbe32c3b17bc1
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2019-03-15 10:29:18 -0700

    Test run, 504 walkers, 5e4 iterations, 50% sparsity

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH --partition=nodes
-#SBATCH -n 300
+#SBATCH -n 504
 #SBATCH --time=30-00:00:00
-#SBATCH --job-name=cnd_10sparse
-#SBATCH --output=cnd_300_10sparse.out
-#SBATCH --error=cnd_300_10sparse.err
+#SBATCH --job-name=cnd_sparse
+#SBATCH --output=cnd_sparse.out
+#SBATCH --error=cnd_sparse.err
 #SBATCH --mail-user=james.casey-clyde@sjsu.edu
 #SBATCH --mail-type=ALL
 
@@ -13,4 +13,4 @@ module purge
 module load intel-python3
 module load mvapich2-2.2/intel
 
-mpiexec python3 mcorbit/main.py --mpi -d ~/MCOrbit/dat/ --nmax 50000 --walkers 300 --out 20190314b -s 0.1
+mpiexec python3 mcorbit/main.py --mpi -d ~/MCOrbit/dat/ --nmax 50000 --walkers 504 --out 20190315 -s 0.5
