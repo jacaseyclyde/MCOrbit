a78f3295fdf61f0272fde358617673a06fb093fa
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2019-03-17 19:47:03 -0700

    Test run, 504 walkers, 5e4 iterations, 50% sparsity, rotationa angle constrained.

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 #SBATCH --partition=nodes
 #SBATCH -n 504
 #SBATCH --time=30-00:00:00
-#SBATCH --job-name=cnd_sparse
-#SBATCH --output=cnd_sparse.out
-#SBATCH --error=cnd_sparse.err
+#SBATCH --job-name=cnd_constrain
+#SBATCH --output=cnd_constrain.out
+#SBATCH --error=cnd_constrain.err
 #SBATCH --mail-user=james.casey-clyde@sjsu.edu
 #SBATCH --mail-type=ALL
 
@@ -13,4 +13,4 @@ module purge
 module load intel-python3
 module load mvapich2-2.2/intel
 
-mpiexec python3 mcorbit/main.py --mpi -d ~/MCOrbit/dat/ --nmax 50000 --walkers 504 --out 20190315 -s 0.5
+mpiexec python3 mcorbit/main.py --mpi -d ~/MCOrbit/dat/ --nmax 50000 --walkers 504 --out 20190317 -s 0.5
