debfab673c7476a0595e43119f91374051f711c4
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2019-03-22 13:40:15 -0700

    Fitting. 504 walkers, 5e5 iterations, 50% data sparsity, 50% of generated orbit points.

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 #SBATCH --partition=nodes
 #SBATCH -n 504
 #SBATCH --time=30-00:00:00
-#SBATCH --job-name=cnd_constrain
-#SBATCH --output=cnd_constrain.out
-#SBATCH --error=cnd_constrain.err
+#SBATCH --job-name=cnd_fit
+#SBATCH --output=cnd_fit.out
+#SBATCH --error=cnd_fit.err
 #SBATCH --mail-user=james.casey-clyde@sjsu.edu
 #SBATCH --mail-type=ALL
 
@@ -13,4 +13,4 @@ module purge
 module load intel-python3
 module load mvapich2-2.2/intel
 
-mpiexec python3 mcorbit/main.py --mpi -d ~/MCOrbit/dat/ --nmax 50000 --walkers 504 --out 20190317 -s 0.5
+mpiexec python3 mcorbit/main.py --mpi -d ~/MCOrbit/dat/ --nmax 500000 --walkers 504 --out 20190322 -s 0.5
