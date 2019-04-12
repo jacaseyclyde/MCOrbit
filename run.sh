1cb5bb11541e4a9a6bc3e07382630b36fdeaaefc
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2019-04-11 19:16:24 -0700

    Running fits for apsides parameter space.

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH --partition=nodes
-#SBATCH -n 1
+#SBATCH -n 252
 #SBATCH --time=30-00:00:00
-#SBATCH --job-name=cnd_plot
-#SBATCH --output=cnd_plot.out
-#SBATCH --error=cnd_plot.err
+#SBATCH --job-name=cnd_apsides
+#SBATCH --output=cnd_apsides.out
+#SBATCH --error=cnd_apsides.err
 #SBATCH --mail-user=james.casey-clyde@sjsu.edu
 #SBATCH --mail-type=ALL
 
@@ -13,4 +13,4 @@ module purge
 module load intel-python3
 module load mvapich2-2.2/intel
 
-mpiexec python3 mcorbit/main.py -d ~/MCOrbit/dat/ --out cnd --plot
+mpiexec python3 mcorbit/main.py --mpi -d ~/MCOrbit/dat/ --nmax 500000 --walkers 252 --out apsides -s 0.5 --sample --plot
