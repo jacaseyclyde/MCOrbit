dc0c5ec07bb2f128e9da0d0a22846c71c5fc928f
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2019-05-06 08:22:22 -0700

    Changing north mask to south mask.

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH --partition=nodes
-#SBATCH -n 504
+#SBATCH -n 126
 #SBATCH --time=30-00:00:00
-#SBATCH --job-name=cnd_apsides
-#SBATCH --output=cnd_apsides.out
-#SBATCH --error=cnd_apsides.err
+#SBATCH --job-name=cnd_south
+#SBATCH --output=cnd_south.out
+#SBATCH --error=cnd_south.err
 #SBATCH --mail-user=james.casey-clyde@sjsu.edu
 #SBATCH --mail-type=ALL
 
