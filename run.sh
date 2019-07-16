4d23071214106b5528ea433f489ce42ecfb01cad
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2019-07-15 18:28:28 -0700

    Changed sampling.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --partition=nodes
-#SBATCH -n 504
-#SBATCH --time=30-00:00:00
+#SBATCH -n 126
+#SBATCH --time=24:00:00
 #SBATCH --job-name=cnd_apsides
 #SBATCH --output=cnd_apsides.out
 #SBATCH --error=cnd_apsides.err
@@ -11,6 +11,7 @@
 module purge
 
 module load intel-python3
-module load mvapich2-2.2/intel
+conda activate mcorbit
+module load mpich/intel
 
-mpiexec python3 mcorbit/main.py --mpi -d ~/MCOrbit/dat/ --nmax 100000 --walkers 504 --out apsides -s 0.5 --sample
+mpiexec python3 mcorbit/main.py --mpi -d ~/MCOrbit/dat/ --nmax 300000 --walkers 504 --out apsides --sample
